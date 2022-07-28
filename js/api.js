4ab1bb0c3a3295ebf1ef5b1922e3055e49b9ce8f
paula garcia <103803748+paulaaleja@users.noreply.github.com>
2022-07-28 03:23:38 -0300

    LocalStorage+Json+Html+Css=Carrito

---
+++ js/api.js
@@ -0,0 +1,82 @@
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'Actron 600 x 20 capsulas',
+        precio: 980,
+        imagen: ''
+    },
+    {
+        id: 2,
+        nombre: 'Aziatop x 10 capsulas',
+        precio: 540,
+        imagen: ''
+    },
+    {
+        id: 3,
+        nombre: 'Dulcolax x 20 grageas',
+        precio: 780,
+        imagen: ''
+    },
+    {
+        id: 4,
+        nombre: 'Coltix gotas x 20ml',
+        precio: 965,
+        imagen: ''
+
+    },
+    {
+        id: 5,
+        nombre: 'Alernix Spray nasal x 100ml',
+        precio: 1200,
+        imagen: ''
+
+    },
+    {
+        id: 6,
+        nombre: 'Curflex x 50 capsulas',
+        precio: 965,
+        imagen: ''
+
+    },
+    {
+        id: 1,
+        nombre: 'Actron 600 x 20 capsulas',
+        precio: 980,
+        imagen: ''
+    },
+    {
+        id: 2,
+        nombre: 'Aziatop x 10 capsulas',
+        precio: 540,
+        imagen: ''
+    },
+    {
+        id: 3,
+        nombre: 'Dulcolax x 20 grageas',
+        precio: 780,
+        imagen: ''
+    },
+    {
+        id: 4,
+        nombre: 'Coltix gotas x 20ml',
+        precio: 965,
+        imagen: ''
+
+    },
+    {
+        id: 5,
+        nombre: 'Alernix Spray nasal x 100ml',
+        precio: 1200,
+        imagen: ''
+
+    },
+    {
+        id: 6,
+        nombre: 'Curflex x 50 capsulas',
+        precio: 965,
+        imagen: ''
+
+    }
+    
+
+];
